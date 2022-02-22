eaaf036aec638a366b2e007aa0b82d88742431af
Sara D'Arce <saradarce@gmail.com>
2022-02-21 21:55:33 -0500

    products and reviews queries

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -7,7 +7,7 @@ DROP TABLE IF EXISTS products;
 
 CREATE TABLE products (
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
+    name VARCHAR(80) NOT NULL,
     url TEXT,
     description TEXT,
     price NUMERIC,
@@ -15,4 +15,15 @@ CREATE TABLE products (
     is_featured BOOLEAN
 );
 
+DROP TABLE IF EXISTS reviews;
 
+CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY,
+    reviewer VARCHAR(40) NOT NULL,
+    title VARCHAR(40) NOT NULL,
+    content TEXT,
+    rating NUMERIC, 
+    CHECK (rating >= 0 AND rating <=5),
+    product_id INTEGER REFERENCES products (id)
+    ON DELETE CASCADE
+);
---
+++ back-end/queries/products.js
@@ -0,0 +1,76 @@
+const db = require("../db/dbConfig.js");
+
+const getAllProducts = async () => {
+  try {
+    const allProducts = await db.any("SELECT * FROM products");
+    return allProducts;
+  } catch (err) {
+    return err;
+  }
+};
+
+const getProduct = async () => {
+  try {
+    const oneProduct = await db.one("SELECT * FROM products WHERE id=$1", id);
+    return oneProduct;
+  } catch (err) {
+    return err;
+  }
+};
+
+const createProduct = async (product) => {
+  try {
+    const newProduct = await db.one(
+      "INSERT INTO products (name, url, description, price, is_featured) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+      [
+        product.name,
+        product.url,
+        product.description,
+        product.price,
+        product.is_featured,
+      ]
+    );
+    return newProduct;
+  } catch (err) {
+    return err;
+  }
+};
+
+const deleteProduct = async (id) => {
+  try {
+    const deletedProduct = await db.one(
+      "DELETE FROM products WHERE id = $1 RETURNING *",
+      id
+    );
+    return deletedProduct;
+  } catch (err) {
+    return err;
+  }
+};
+
+const updateProduct = async (id, product) => {
+  try {
+    const updatedProduct = await db.one(
+      "UPDATE products SET name=$1, url=$2, description=$3, price=$4, is_featured=$5 WHERE id=$6 RETURNING *",
+      [
+        product.name,
+        product.url,
+        product.description,
+        product.price,
+        product.is_featured,
+        id,
+      ]
+    );
+    return updatedProduct;
+  } catch (err) {
+    return err;
+  }
+};
+
+module.exports = {
+  getAllProducts,
+  getProduct,
+  createProduct,
+  deleteProduct,
+  updateProduct,
+};
